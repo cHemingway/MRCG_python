ce88cdd22577b1d5d34ecb74315193bd0e5e388c
Chris Hemingway <chris.hemingway@gmail.com>
2019-07-29 17:33:22 +0100

    Flatten directory hierarchy
    Makes imports simpler, and remove duplicate files while we are at it

--- test_MRCG.py
+++ test_MRCG.py
@@ -1,18 +1,19 @@
 import tensorflow as tf
 import numpy as np
-import mrcg.MRCG as mrcg
+import MRCG
 import scipy.io.wavfile
 import os
 import librosa
 import wave
 import time
 
-wav_dir = './../example/SNR103F3MIC021002_ch01.wav'
+script_path = os.path.dirname(os.path.abspath( __file__ ))
+wav_dir = os.path.join(script_path,'example/SNR103F3MIC021002_ch01.wav')
 audio, sr = librosa.load(wav_dir, sr=16000)
 # sr,audio = scipy.io.wavfile.read(wav_dir)
 print('success to load sample wav-file')
 s = time.clock()
-samp_mrcg = mrcg.mrcg_extract(audio,sr)
+samp_mrcg = MRCG.mrcg_extract(audio,sr)
 e = time.clock()
 print('success to extract features')
 print(e-s)
